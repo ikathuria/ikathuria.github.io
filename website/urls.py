7ec2bb13f402a3c9173b7bd7f70f6e6d9ce9428c
ikathuria <ishani@kathuria.net>
2021-09-21 13:25:02 +0530

    renamed application

--- website/urls.py
+++ website/urls.py
@@ -1,4 +1,4 @@
-"""my-website URL Configuration
+"""website URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
