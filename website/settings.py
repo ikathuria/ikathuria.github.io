2fe9927cfe5b36b39739fc161d9ffd74cf325378
ikathuria <ishani@kathuria.net>
2021-09-21 15:22:33 +0530

    whitenoise update

--- website/settings.py
+++ website/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
